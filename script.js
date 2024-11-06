1e8abb8fce4c543124ec84df7cd5b65b58bbaacd
Neimar Jr <neimar@localhost.localdomain>
2024-11-06 12:30:17 -0300

    ajustes na interface e criacao da funcao de gerar o deck e embaralhar o deck

--- script.js
+++ script.js
@@ -15,13 +15,61 @@ const values = [
 ];
 
 let deck = [];
+let playerHand = [];
+let computerHand = [];
+let trunfoCard;
+let playerScore = 0;
+let computerScore = 0;
+
 
 // Função para criar o baralho
-function createDeck() {
+const createDeck = () => {
     deck = [];
     for (let suit of suits) {
         for (let value of values) {
             deck.push({ suit: suit, ...value });
         }
     }
+}
+
+// Função para embaralhar o baralho
+function shuffleDeck() {
+    for (let i = deck.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [deck[i], deck[j]] = [deck[j], deck[i]];
+    }
+}
+
+
+window.onload = function() {
+    function displayTrunfoCard(trunfo) {
+        const trunfoElement = document.getElementById("trunfo-card-front");
+        trunfoElement.innerHTML = "<div class='card-value'>"+trunfo['name']+"</div><div class='card-suit'>"+getSuitSymbol(trunfo['suit'])+"</div>";
+    }
+
+    // Supondo que você já tenha definido o trunfo, exiba-o:
+    const trunfo = { name: "A", suit: "Ouros" }; // Exemplo de trunfo
+    displayTrunfoCard(trunfo);
+
+    // Função para obter o símbolo do naipe
+    function getSuitSymbol(suit) {
+        switch (suit) {
+            case "Copas": return "♥";
+            case "Espadas": return "♠";
+            case "Ouros": return "♦";
+            case "Paus": return "♣";
+            default: return "";
+        }
+    }
+}
+
+
+document.getElementById("start-game").addEventListener("click", startGame);
+
+// Função para iniciar o jogo
+function startGame() {
+    createDeck();
+    shuffleDeck();    
+    playerScore = 0;
+    computerScore = 0;
 }
